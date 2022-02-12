0ebf1a1ef66b63ecad2a7bad415514b276720b79
Kerem Büyükdereli <kerembykdrl@gmail.com>
2022-02-12 11:40:00 +0300

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+  };
+  console.log(word);
   return (
     <div className="App">
-      <Header title = "WebDevelopmentSelfStudy"/>
-      <Search />
+      <Header title="WebDevelopmentSelfStudy" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
-            <Form.Row >
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
               <Col>
                 <Form.Label>Enter here</Form.Label>
               </Col>
             </Form.Row>
             <Form.Row>
               <Col xs={8}>
-                <Form.Control placeholder="Search" />
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search"
+                />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
