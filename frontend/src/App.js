8958f6ee9230325ab833e275cb83d7a86404a67b
KeremB <ARBYL-EI.NT_Project@arcelik.com>
2022-02-14 16:01:26 +0300

    reset search input upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
+
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState("");
@@ -9,8 +13,19 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(e.target[0].value);
+    fetch('https://api.unsplash.com/photos/random/?query='+word+'&client_id='+UNSPLASH_KEY)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    setWord('')
   };
-  console.log(word);
+  
+  
+
   return (
     <div className="App">
       <Header title="WebDevelopmentSelfStudy" />
