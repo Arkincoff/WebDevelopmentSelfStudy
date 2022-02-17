d3ca34a5a5b05e3291b4facd420267789d2470bd
Kerem Büyükdereli <kerembykdrl@gmail.com>
2022-02-17 19:09:49 +0300

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +23,7 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
